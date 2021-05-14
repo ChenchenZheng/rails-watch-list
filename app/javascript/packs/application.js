61448dcd9227975017264c9f63f3661aac432f7a
Chenchen Zheng <chenchenzheng42@gmail.com>
2021-05-14 16:32:36 +0200

    ✨ Add star rating for review

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,6 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import 'bootstrap';
+import { initStarRating } from '../plugins/init_star_rating';
+
+initStarRating();
